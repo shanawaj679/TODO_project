50d3adb9c1fd784bb7daf33070430dcea21dd5be
SHANAWAJ NADAF <147726542+shanawaj679@users.noreply.github.com>
2026-07-27 20:34:24 +0530

    login frontend is updated

--- to-do_project/src/login/login.jsx
+++ to-do_project/src/login/login.jsx
@@ -2,6 +2,7 @@ import "./login.css"
 import {motion} from "framer-motion"
 import { useState,useEffect } from "react"
 import axios from "axios"
+import {Link} from "react-router-dom"
 function Login(){
   const [name,setname]=useState("")
   const [email,setemail]=useState("")
@@ -23,18 +24,18 @@ const fadeup={initial:{opacity:0,scale:0.95},
           </header>
           <main>
              <div>
-               <motion.div {...fadeup} transition={{...fadeup.transition,duration:0.75}} className="log_label">Name : </motion.div>
-              <motion.div {...fadeup} transition={{...fadeup.transition,duration:0.8}}><input type="text" placeholder="name" value={name} onChange={(e)=>{setname(e.target.value)}} className="log_input"></input></motion.div>
                <motion.div {...fadeup} 
 transition={{...fadeup.transition,duration:0.85}} className="log_label">Email : </motion.div>
               <motion.div {...fadeup} transition={{...fadeup.transition,duration:0.9}}><input type="email" placeholder="shanawaj@gmail.com" value={email} onChange={(e)=>{setemail(e.target.value)}} className="log_input"></input></motion.div>
                <motion.div {...fadeup} 
 transition={{...fadeup.transition,duration:0.95}} className="log_label">Password : </motion.div>
               <motion.div {...fadeup} transition={{...fadeup.transition,duration:1}}><input type="password" placeholder="password" value={password} onChange={(e)=>{setpassword(e.target.value)}} className="log_input"></input></motion.div>
-               <motion.div {...fadeup} 
-transition={{...fadeup.transition,duration:1.05}} className="log_label">Confirm Password : </motion.div>
-              <motion.div {...fadeup} transition={{...fadeup.transition,duration:1.1}}><input type="text" placeholder="password" value={confirmpassword} onChange={(e)=>{setconfirmpassword(e.target.value)}} className="log_input"></input></motion.div>
-              
+              <motion.div ><Link className="forgot_btn" to="/forgetpassword">🔒 Forget Password</Link></motion.div>
+              <motion.div> <motion.button whileHover={{ scale: 1.03, y: -2 }}
+               whileTap={{ scale: 0.97 }} 
+               className="login_btn">Login</motion.button> </motion.div>
+              <motion.div >  Don't have an account? <Link className="links" to="/register">Sign In</Link></motion.div>
+            
              </div>
           </main>
         </div>
