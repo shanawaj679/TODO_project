a5d62d0464e718065b5930acb2a2dc9634e1ddcb
SHANAWAJ NADAF <147726542+shanawaj679@users.noreply.github.com>
2026-07-27 20:34:33 +0530

    routes are added

--- to-do_project/src/App.jsx
+++ to-do_project/src/App.jsx
@@ -1,8 +1,17 @@
 import "./App.css"
+import {BrowserRouter, Routes,Route } from "react-router-dom";
 import Login from "./login/login.jsx"
+import Register from "./register/register.jsx";
+import Forgetpassword from "./forgetpassword/forgetpassword.jsx";
 function App(){
     return <>
-      <Login />
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/forgetpassword" element={<Forgetpassword />} />
+      </Routes>
+      </BrowserRouter>
     </>
 }
 export default App;
